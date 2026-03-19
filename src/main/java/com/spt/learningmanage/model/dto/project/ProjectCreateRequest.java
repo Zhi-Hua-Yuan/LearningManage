7583fc5802744da5b785463c3646666dc3653da2
SongPintong <1215787625@qq.com>
2026-03-19 23:17:18 +0800

    fix: 修复创建项目时日期格式解析失败的 Bug

--- src/main/java/com/spt/learningmanage/model/dto/project/ProjectCreateRequest.java
+++ src/main/java/com/spt/learningmanage/model/dto/project/ProjectCreateRequest.java
@@ -1,5 +1,6 @@
 package com.spt.learningmanage.model.dto.project;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 
 import java.time.LocalDate;
@@ -10,6 +11,9 @@ public class ProjectCreateRequest {
 
     private String goal;
 
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss") // 告诉后端：如果看到带时间的格式，自动帮我转成 LocalDate
     private LocalDate startDate;
+
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDate endDate;
 }
