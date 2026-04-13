b5f145d5bd2dba67e926cf59b01450c692c7c4d0
SongPintong <154807443+Zhi-Hua-Yuan@users.noreply.github.com>
2026-04-13 11:13:22 +0800

    Delete sql/alter_project_add_order_no.sql

--- sql/alter_project_add_order_no.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE `project`
-    ADD COLUMN `order_no` INT NOT NULL DEFAULT 0 COMMENT '排序号，从0开始，越小越靠前' AFTER `status`;
-
-CREATE INDEX `idx_project_user_order_no` ON `project` (`user_id`, `order_no`);
-
